0b0ab562d0028c50fcfcab2ffc10f166a3f47018
Lara Fons <larafons94@gmail.com>
2023-05-12 15:00:49 -0300

    limit agregado

--- entregaClase6/src/app.js
+++ entregaClase6/src/app.js
@@ -1,4 +1,3 @@
-const { application } = require("express")
 const express = require('express')
 const ProductManager = require('../../entregaClase4/desafio2Archivos').default;
 
@@ -7,11 +6,17 @@ const productManager = new ProductManager();
 
 app.listen(8080, ()=> console.log('Servidor 8080 levantado'))
 
-app.get('/products', (req, res) => { //agregar el limit
-    res.send(productManager.obtenerProductos());
+app.get('/products', async (req, res) => { // ?limit=x
+    let limit = req.query.limit;
+    if (!limit){
+        res.send(productManager.obtenerProductos());
+    }
+    else {
+        res.send(productManager.obtenerProductos().slice(0, limit));
+    }
 });
 
-app.get('/products/:pid', (req, res) => {
+app.get('/products/:pid', async (req, res) => {
     let productId = req.params.pid;
     res.send(productManager.consultarProducto(productId))
 });
